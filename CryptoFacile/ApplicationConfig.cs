a661cb8b89a1faecfd4c3d769d549058bbc9c345
Nirad <miljours@gmail.com>
2021-07-14 22:30:46 -0400

    Fenêtre Pool
    Ajout d'une liste box pour les pool

--- CryptoFacile/ApplicationConfig.cs
+++ CryptoFacile/ApplicationConfig.cs
@@ -5,11 +5,10 @@ namespace CryptoFacile
 {
     public class ApplicationConfig
     {
-        public string XHVWalet = "";
-        public int HashGive = 75;
-        public int CpuSpeed = 70;
+        public int HashGive = 10;
+        public int CpuSpeed = 75;
         public bool CpuActive = true;
-        public bool GpuActive = false;
+        public bool GpuActive = true;
         public bool IsLess4GB = false;
         private IniFile ConfFile = new IniFile("Reglages.ini");
 
@@ -21,19 +20,15 @@ namespace CryptoFacile
 
         public void Load()
         {
-            XHVWalet = ConfFile.Read("XHVWalet");
-            HashGive = int.TryParse(ConfFile.Read("HashGive"), out HashGive) ? HashGive : 100;
+            HashGive = int.TryParse(ConfFile.Read("HashGive"), out HashGive) ? HashGive : 10;
             CpuSpeed = int.TryParse(ConfFile.Read("CpuSpeed"), out CpuSpeed) ? CpuSpeed : 70;
             CpuActive = bool.TryParse(ConfFile.Read("CpuActive"), out CpuActive) ? CpuActive : true;
             GpuActive = bool.TryParse(ConfFile.Read("GpuActive"), out GpuActive) ? GpuActive : true;
             IsLess4GB = bool.TryParse(ConfFile.Read("IsLess4GB"), out IsLess4GB) ? IsLess4GB : false;
-            if (HashGive < 15)
-                HashGive = 100;
         }
 
         public void Save()
         {
-            ConfFile.Write("XHVWalet", XHVWalet);
             ConfFile.Write("HashGive", HashGive.ToString());
             ConfFile.Write("CpuSpeed", CpuSpeed.ToString());
             ConfFile.Write("CpuActive", CpuActive.ToString());
--- CryptoFacile/Window/Wallet.xaml.cs
+++ CryptoFacile/Window/Wallet.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows;
-
+using System.Windows.Input;
 
 namespace CryptoFacile
 {
@@ -10,9 +10,8 @@ namespace CryptoFacile
     public partial class Wallet : Window
     {
         private int _Progress;
-        private ApplicationConfig _AppConf = new ApplicationConfig();
         private bool IsRunning = false;
-        string pwallet = "";
+        App _app = ((App)System.Windows.Application.Current);
 
         public int Progress
         {
@@ -26,15 +25,8 @@ namespace CryptoFacile
         {
             InitializeComponent();
             IsRunning = isRunning;
-            _AppConf = appConfig;
-            SL2.Value = _AppConf.HashGive;
-            pwallet = _AppConf.XHVWalet;
-            if (_AppConf.XHVWalet.Length > 24)
-                txt_XMR.Text = _AppConf.XHVWalet.Substring(0, 24);
-            //txtETH.Text = _AppConf.ETHWalet;
-           //txtETC.Text = _AppConf.ETCWalet;
-
-            Dev();
+            SL2.Value = _app.AppConf.HashGive;
+            listPools.ItemsSource = _app.PoolList;
 
             if (IsRunning)
                 DisableControl();
@@ -43,7 +35,6 @@ namespace CryptoFacile
         private void DisableControl()
         {
             SL2.IsEnabled = false;
-            txt_XMR.IsEnabled = false;
         }
 
         private void SL2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -55,37 +46,30 @@ namespace CryptoFacile
             }
             int hashGive = (int)e.NewValue;
             txtslider2.Content = $"{hashGive} %";
-            Dev();
-        }
-
-        private void Dev()
-        {
-            if (_AppConf.XHVWalet.ToLower() == "nirad")
-            {
-                SL2.Value = 0;
-                SL2.Minimum = 0;
-                _Progress = 0;
-                SL2.IsEnabled = false;
-            }
         }
 
         private void Window_Closed(object sender, System.EventArgs e)
         {
             if(!IsRunning && ((Window)sender).IsLoaded)
             {
-                ((App)System.Windows.Application.Current).AppConf.HashGive = (int)SL2.Value;
-                if(string.IsNullOrEmpty(_AppConf.XHVWalet))
-                    if (txt_XMR.Text.Length > 24)
-                        ((App)System.Windows.Application.Current).AppConf.XHVWalet = txt_XMR.Text;
-                if (_AppConf.XHVWalet.Length > 24)
-                    if (!_AppConf.XHVWalet.Substring(0, 24).Equals(txt_XMR.Text))
-                        ((App)System.Windows.Application.Current).AppConf.XHVWalet = txt_XMR.Text;
+                _app.AppConf.HashGive = (int)SL2.Value;
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Process.Start(new Uri("https://accounts.binance.com/fr/register?ref=75547280").AbsoluteUri);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }
